bb3e2092b1ec9306ddb1595553487e67eed53339
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-11-02 21:05:02 -0800

    week 3

--- week-3/assignment-3.2/NodeSecurity-nosql-data-structure.js
+++ week-3/assignment-3.2/NodeSecurity-nosql-data-structure.js
@@ -6,37 +6,33 @@ const user = {
     link_user_role: [
       {
         _id: "5f680fd78db0f818c3fe7a80",
-        role: [
-          {
-            _id: "5f681826c78dee1a48a6e0db",
-            role_type: "ROLE_ADMIN",
-            link_role_permission: [
-              {
-                _id: "5f681652f0330a19bc97d119",
-                permission: [
-                  {
-                    _id: "5f6816577sh3d3237d11987",
-                    resource: "users",
-                    allow_action: ["edit", "create", "delete", "get", "list"],
-                  },
-                  {
-                    _id: "5f6816577djeif372939484",
-                    resource: "posts",
-                    allow_action: ["edit", "create", "delete", "get", "list"],
-                  },
-                ],
+        responsibility: "All permission granted",
+        role: {
+          _id: "5f681826c78dee1a48a6e0db",
+          role_type: "ROLE_ADMIN",
+          link_role_permission: [
+            {
+              _id: "5f681652f0330a19bc97d119",
+              permission: {
+                _id: "5f6816577sh3d3237d11987",
+                resource: "users",
+                allow_action: ["edit", "create", "delete", "get", "list"],
               },
-            ],
-          },
-          {
-            _id: "5f681826c78dee1a48a6e0db",
-            role_type: "ROLE_EMPLOYEE",
-            link_role_permission: [],
-          },
-        ],
+            },
+            {
+              _id: "5f681652f0330a19bc97d119",
+              permission: {
+                _id: "5f6816577djeif372939484",
+                resource: "posts",
+                allow_action: ["edit", "create", "delete", "get", "list"],
+              },
+            },
+          ],
+        },
       },
       {
         _id: "5f680fd78db076545dyey7364",
+        responsibility: "Post granted | User read",
         role: [
           {
             _id: "5f76dc91a007ac0017bf74ab",
@@ -44,18 +40,19 @@ const user = {
             link_role_permission: [
               {
                 _id: "5f681652f0330a19b7sh37d6",
-                permission: [
-                  {
-                    _id: "5f6816577sh3d3237d9387bd",
-                    resource: "users",
-                    allow_action: ["get", "list"],
-                  },
-                  {
-                    _id: "636482577sh3d3239dj473g3",
-                    resource: "posts",
-                    allow_action: ["create", "get", "list"],
-                  },
-                ],
+                permission: {
+                  _id: "5f6816577sh3d3237d9387bd",
+                  resource: "users",
+                  allow_action: ["get", "list"],
+                },
+              },
+              {
+                _id: "5f681652f0330a19b7sh37d6",
+                permission: {
+                  _id: "636482577sh3d3239dj473g3",
+                  resource: "posts",
+                  allow_action: ["create", "get", "list"],
+                },
               },
             ],
           },
---
+++ week-3/assignment-3.3/NodeBookCo-nosql-data-structure.js
@@ -0,0 +1,61 @@
+const publisher = {
+  publisher: {
+    _id: "5f680faf5330d6189a73e024",
+    name: "Fullstack.IO",
+    code: "1012467",
+    address: "West Hollywood, CA",
+    book: [
+      {
+        _id: "5f680fd78db0f818c3fe7a80",
+        name: "Full Stact React",
+        isbn: "80825236",
+        price: "59.99",
+        author: [
+          {
+            _id: "5f681826c78dee1a48a6e0db",
+            first_name: "Nate",
+            last_name: "Murray",
+            phone: "323 456-5675",
+            email: "nmurray@fullstack.io",
+          },
+          {
+            _id: "5f6816577sh3d3237d11987",
+            first_name: "Anthony",
+            last_name: "Accomazzo",
+            phone: "323 456-5675",
+            email: "aaccomazzo@fullstack.io",
+          },
+          {
+            _id: "5f681826c78dee1a48a6e0db",
+            first_name: "Clay",
+            last_name: "Allsopp",
+            phone: "323 456-5675",
+            email: "callsopp@fullstack.io",
+          },
+        ],
+      },
+      {
+        _id: "5f680fd78db0heyeh3746280",
+        name: "Fullstack D3 and Data Visualization",
+        isbn: "80785097",
+        price: "79.99",
+        author: [
+          {
+            _id: "5f681826c78d6354820998712",
+            first_name: "Florian",
+            last_name: "Grassinger",
+            phone: "323 102-5675",
+            email: "fgrassinger@fullstack.io",
+          },
+          {
+            _id: "5f6816577shasdf8282737382",
+            first_name: "Leandro",
+            last_name: "Collares",
+            phone: "323 456-5675",
+            email: "lcollares@fullstack.io",
+          },
+        ],
+      },
+    ],
+  },
+};
