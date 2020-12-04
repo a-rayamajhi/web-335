9102d02131b27e0e85c577f97272d285455a6b63
Anil Rayamajhi <anilrayamajhi@Anils-iMac.local>
2020-12-03 20:01:15 -0800

    week 8

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ WEB335-330H Introduction to NoSQL (2211-DD)
 
 # contributors
 
-> Professor Massoud - Bellevue University
+> Professor Krasso - Bellevue University
 >
 > Anil Rayamajhi - Bellevue University
 
---
+++ week-8/rayamajhi_hello_world.py
@@ -0,0 +1,10 @@
+# ===========================================
+# Title: Exercise 8.2
+# Author: Professor Krasso
+# Date: 3 December 2020
+# Modified by: Anil Rayamajhi
+# Description: Anaconda | Anaconda Navigator | Python3 script | Debug
+# ==========================================
+#
+
+print("Hello World")
